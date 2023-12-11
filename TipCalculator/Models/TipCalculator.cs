c1af78cf8ece2782cca505911edb049aa87ee78e
emalyovany <54923708+emalyovany@users.noreply.github.com>
2023-12-10 23:44:18 -0500

    Update TipCalculator.cs

--- TipCalculator/Models/TipCalculator.cs
+++ TipCalculator/Models/TipCalculator.cs
@@ -5,18 +5,20 @@ namespace TipCalculator.Models
     public class Calculator
     {
         [Required(ErrorMessage = "Please enter a value for cost of meal.")]
-        [Range(0.0, 10000000.0, ErrorMessage = "Cost of meal must be greater than zero.")]
+        [Range(0.01, 10000000.0, ErrorMessage = "Cost of meal must be greater than zero.")]
         public double? MealCost { get; set; }
 
         public double CalculateTip(double percent)
         {
-            if (MealCost.HasValue)
+            if (MealCost.HasValue && percent > 0)
             {
-                var tip = MealCost.Value / percent;
+                // Assuming percent is passed as a decimal (e.g., 15% is passed as 0.15)
+                var tip = MealCost.Value * percent;
                 return tip;
             }
             else
             {
+                // Return 0 if MealCost is not set or percent is not greater than 0
                 return 0;
             }
         }
